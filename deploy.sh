371c99c55c6e83963ae92e8fa0df3e132790a946
FelipeBarrosCruz <felipe.cruz@hash.com.br>
2022-02-03 20:32:03 -0300

    ci: added step to deploy the application

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@ TAG_VERSION="latest"
 PROJECT_ID=$(gcloud config get-value project)
 REGION="us-east1"
 
+gcloud auth configure-docker -q
+gcloud auth configure-docker ${REGION}-docker.pkg.dev
+gcloud config set artifacts/repository ${SERVICE_NAME}
+gcloud config set artifacts/location ${REGION}
+
 IMAGE_TAG_URL=${REGION}-docker.pkg.dev/${PROJECT_ID}/${SERVICE_NAME}/${SERVICE_NAME}:${TAG_VERSION}
 
 docker build -t ${SERVICE_NAME}:${TAG_VERSION} .
